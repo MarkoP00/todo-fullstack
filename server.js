c2bc045999942a28a999d530c600d7004d33f828
MarkoP00 <marebrat789@gmail.com>
2025-05-14 19:22:58 +0200

    Fixing CORS

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "https://todo-fullstack-0eh6.onrender.com",
+      "https://glittering-toffee-d0add8.netlify.app",
     ],
     credentials: false,
     methods: ["GET", "POST", "PATCH", "DELETE"],
